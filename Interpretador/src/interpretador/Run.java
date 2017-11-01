edffdedcf874862e18ba6260310c2f651768aa31
André Luiz Lima <andre.luiz.lima.1997@gmail.com>
2017-11-01 08:16:13 -0200

    Vis

--- Interpretador/src/interpretador/Run.java
+++ Interpretador/src/interpretador/Run.java
@@ -42,10 +42,10 @@ public class Run {
     private static void showParseTreeFrame(ParseTree tree, InterpretadorParser parser) throws HeadlessException {
         JFrame frame = new JFrame("SRC: " + tree.getText());
         JPanel panel = new JPanel();
-        TreeViewer viewr = new TreeViewer(Arrays.asList(
+        TreeViewer viewer = new TreeViewer(Arrays.asList(
         parser.getRuleNames()), tree);
-        viewr.setScale(1);
-        panel.add(viewr);
+        viewer.setScale(1);
+        panel.add(viewer);
         frame.add(panel);
         frame.setSize(1000, 600);
         frame.setState(JFrame.MAXIMIZED_HORIZ);
